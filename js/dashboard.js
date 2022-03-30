30651a4b074054066cc24af914ecde9134ce3052
seguna15 <seguna15@gmail.com>
2022-03-30 11:52:50 +0100

    logo

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,11 +2,13 @@
 const openMenu = document.querySelector('.open-menu');
 const navBar = document.querySelector('.navbar');
 const closeMenu = document.querySelector('.close-menu');
+const logo = document.querySelector('.logo');
 
 openMenu.addEventListener('click', () => {
     navBar.classList.add('active');
-    closeMenu.style.display= "inline-flex";
+    closeMenu.style.display= "flex";
     openMenu.style.display = 'none';
+    logo.style.display = 'none';
 
 });
 
@@ -14,4 +16,5 @@ closeMenu.addEventListener('click', () => {
     navBar.classList.remove('active');
     closeMenu.style.display= "none";
     openMenu.style.display = 'flex';
+    logo.style.display = 'flex';
 })
